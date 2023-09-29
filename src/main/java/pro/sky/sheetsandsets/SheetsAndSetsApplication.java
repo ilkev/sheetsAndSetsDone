f3d0342898548dfbb4b7d43ef0a64030aefcf5a9
incev <ilyaka89@gmail.com>
2023-09-30 00:59:19 +0300

    initial commit

---
+++ src/main/java/pro/sky/sheetsandsets/SheetsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.sheetsandsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetsAndSetsApplication.class, args);
+	}
+
+}
